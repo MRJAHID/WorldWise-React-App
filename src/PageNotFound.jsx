2e31f28188c824ce8d631f2c3f63e73b84cd4ecf
MRJAHID <mrjahid350@gmail.com>
2024-01-23 16:58:32 +0600

    Design

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PageNotFound = () => {
     return (
         <div>
-            PageNotFound
+            <h1>Page not found 😢</h1>
         </div>
     );
 };
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+// Uses the same styles as Product
+import styles from "./Product.module.css";
 
-const Pricing = () => {
-    return (
+export default function Product() {
+  return (
+    <main className={styles.product}>
+      <section>
         <div>
-            Pricing
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
         </div>
-    );
-};
-
-export default Pricing;
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
+  );
+}
