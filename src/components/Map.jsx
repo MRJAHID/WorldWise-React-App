b3beccc9be524221860b04af7701d73c4e0aac69
MRJAHID <mrjahid350@gmail.com>
2024-01-28 14:10:25 +0600

    Fetching City Data in the Form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
     MapContainer,
     TileLayer,
@@ -10,19 +10,21 @@ import {
 
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
-import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
-import { useUrlPosition } from "../hooks/useUrlPosition.jsx";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 import Button from "./Button";
+import {useCities} from "../context/CitiesContext.jsx";
 
 function Map() {
     const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([40, 0]);
+
     const {
         isLoading: isLoadingPosition,
         position: geolocationPosition,
         getPosition,
     } = useGeolocation();
+
     const [mapLat, mapLng] = useUrlPosition();
 
     useEffect(
