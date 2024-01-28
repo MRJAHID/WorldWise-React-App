781ae5d236e6eac8125f9a5d8ff25ef1fe500b17
MRJAHID <mrjahid350@gmail.com>
2024-01-28 20:55:09 +0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
 import {CitiesProvider} from "./context/CitiesContext.jsx";
 import {AuthProvider} from "./context/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 
 
 function App() {
@@ -22,7 +23,11 @@ function App() {
                         <Route index element={<Homepage/>}/>
                         <Route path='product' element={<Product/>}/>
                         <Route path='pricing' element={<Pricing/>}/>
-                        <Route path='app' element={<AppLayout/>}>
+                        <Route path='app' element={
+                            <ProtectedRoute>
+                                <AppLayout/>
+                            </ProtectedRoute>
+                        }>
                             <Route index element={<Navigate replace to='cities'/>}/>
                             <Route path='cities' element={<CityList/>}/>
                             <Route path='cities/:id' element={<City/>}/>
